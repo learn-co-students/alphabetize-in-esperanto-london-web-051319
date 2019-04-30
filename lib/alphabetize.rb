44715f8d8d888d878308358ced52319399ea2916
github username <github email address>
2019-04-30 17:39:13 +0200

    alphabetize done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+require "pry"
 def alphabetize(arr)
-  # code here
+	new_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+	
+  	arr.sort_by! do |a|
+  	# ! changes original arr, it will sort by the index of the new_alphabet
+    positions = []
+    # array of each character's indexes
+    	a.each_char do |char|
+      		positions << new_alphabet.index(char)
+    	# for each word 'a' we take each character and push the index to positions
+    	end
+    	positions
+  	end
+  	arr
+
 end
