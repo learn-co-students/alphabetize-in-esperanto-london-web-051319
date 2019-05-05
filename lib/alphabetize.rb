75ef8a81afc08296d60bd4217cd3005454052cfd
github username <github email address>
2019-05-05 23:06:50 +0100

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  # arr.join("").split("").compact.uniq!
+
+  # binding.pry
+
+  first_letter = arr.collect {|element| element[0]}
+  indexes = first_letter.collect {|letter| alphabet.index(letter)}
+  order_hash = {}
+  alphabetized_array= []
+  i = 0
+  until i == indexes.length
+    order_hash[arr[i]] = indexes[i]
+    i+=1
+  end
+  indexes.sort.each do |num|
+    order_hash.each do |phrase, val|
+      if num == val
+        alphabetized_array << phrase
+      end
+    end
+  end
+  alphabetized_array
 end
